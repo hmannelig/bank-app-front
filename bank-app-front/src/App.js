8632401309ad08120661f211b4260b79416b88c6
Pablo Berjon <2490772B@student.gla.ac.uk>
2023-04-23 22:39:20 +0100

    refactor code by moving routing to Main.js class

--- bank-app-front/src/App.js
+++ bank-app-front/src/App.js
@@ -1,56 +1,12 @@
 import "./App.css";
-import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
-import About from "./components/pages/about/About";
-import Bank from "./components/pages/bank/Bank";
-import Login from "./components/pages/login/Login";
-import Home from "./components/pages/home/Home";
-import SignUp from "./components/pages/signup/SignUp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/home" className="custom-link">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/login" className="custom-link">
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link to="/signup" className="custom-link">
-                  Sign-up
-                </Link>
-              </li>
-              <li>
-                <Link to="/bank" className="custom-link">
-                  Bank
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="custom-link">
-                  About
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/bank" element={<Bank />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+        <Main />
       </header>
-      {/* <Main /> */}
     </div>
   );
 }
--- bank-app-front/src/components/Main.js
+++ bank-app-front/src/components/Main.js
@@ -1,14 +1,53 @@
-// import React from "react";
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
-// import About from "./pages/about/About";
-// import Bank from "./pages/bank/Bank";
-// import Login from "./pages/login/Login";
+import About from "./pages/about/About";
+import Bank from "./pages/bank/Bank";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
 
-// const Main = () => {
-//   return (
+function Main() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home" className="custom-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="custom-link">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="custom-link">
+              Sign-up
+            </Link>
+          </li>
+          <li>
+            <Link to="/bank" className="custom-link">
+              Bank
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="custom-link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/bank" element={<Bank />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-//   );
-// };
-
-// export default Main;
+export default Main;
